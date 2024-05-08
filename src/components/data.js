267816cff3db2f6ef1cc80a57cdafc36c4798d3d
Umair <umairbaig995@gmail.com>
2024-05-08 20:59:24 +0500

    ui design in process

---
+++ src/components/data.js
@@ -0,0 +1,372 @@
+export const data = {
+    variation_theme: [
+      {
+        name: "STYLE_NAME",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    contains_liquid_contents: [
+      {
+        value: "true",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    item_package_weight: [
+      {
+        unit: "grams",
+        value: "132.00",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    active_ingredients: [
+      {
+        value:
+          "Vitamin A 800ug, Vitamin D 0.75ug, Vitamin C 12mg, Niacinamide 16mg, Hyaluronic acid 10mg.",
+        language_tag: "en_GB",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    fulfillment_availability: [
+      {
+        fulfillment_channel_code: "AMAZON_EU",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    manufacturer: [
+      {
+        language_tag: "en_GB",
+        value: "Vitrition",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    safety_warning: [
+      {
+        language_tag: "en_GB",
+        value:
+          "Do not exceed recommended daily intake. Excessive consumption may cause a laxative effect.",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    externally_assigned_product_identifier: [
+      {
+        value: "5060996080016",
+        type: "ean",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    recommended_browse_nodes: [
+      {
+        value: "344266031",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    bullet_point: [
+      {
+        language_tag: "en_GB",
+        value:
+          "ANTI AGING SKIN SUPERFOOD \u2013 We fused anti-inflammatory agents with antioxidants to produce a multi-serving, anti-aging serum that reduces fine lines, wrinkles, dark spots, and skin inflammation, while moisturising and hydrating skin for a plumper feel and healthier, younger complexion.",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+      {
+        language_tag: "en_GB",
+        value:
+          "PROTECTS & STRENGTHENS SKIN \u2013 Our expert ingredients of Turmeric, Retinol/Vitamin A, Hyaluronic Acid and Collagen are proven to protect skin from radical damage that contributes to premature aging, unclog pores/bumps/rough patches, prevent acne, increase skin firmness and elasticity, and maintain water retention.",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+      {
+        language_tag: "en_GB",
+        value:
+          "REVIVE EDIBLE SKINCARE SERUM - As the condition of our skin reflects our body\u2019s inner health, our products are designed to surpass a topical cover up and meet the root of skin conditions that allows for a greater and easier absorption of expert ingredients to heal, restore, and maintain youthful, vibrant skin.",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+      {
+        language_tag: "en_GB",
+        value:
+          "EASY AND DELICIOUS SKINCARE REGIME \u2013 Incorporate our tasty Peachy Punch flavoured serum into your beauty regime through one easy step (30 day serving). Shake well before use. Using the pipette, squeeze 1.4ml on to the tongue and swallow. Do not exceed recommended daily intake.",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+      {
+        language_tag: "en_GB",
+        value:
+          "ENHANCED COMPLEXION FOR ALL SKIN TYPES \u2013 Our \u2018inside out\u2019 holistic remedy supports all skin types to reverse internal and external skin complications by healing uneven texture through nourishment and hydration to restore a smoother and happier consistency!",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    ingredients: [
+      {
+        language_tag: "en_GB",
+        value:
+          "Water, Humectant (Glycerol), Apple Juice Concentrate, Turmeric Liquid Extract, Hydrolysed Bovine Collagen, Niacinamide, Sodium Ascorbate, Natural Flavourings, Zinc Gluconate, Hyaluronic Acid, Acidity Regulator (Malic Acid), Sweetener (Steviol Glycosides), Preservatives (Potassium Sorbate, Sodium Benzoate), Stabiliser (Carboxymethylcellulose), Retinyl Acetate, Cholecalciferol.",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    item_dimensions: [
+      {
+        height: {
+          unit: "centimeters",
+          value: "12.20",
+        },
+        length: {
+          unit: "centimeters",
+          value: "3.80",
+        },
+        width: {
+          unit: "centimeters",
+          value: "5.80",
+        },
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    product_description: [
+      {
+        language_tag: "en_GB",
+        value:
+          "Peach flavour vitamin and mineral syrup food supplement with sweetener, hydrolysed bovine collagen, turmeric extract and hyaluronic acid. Food supplements should not be used as a substitute for a varied diet and healthy lifestyle. If on medication, please consult a health professional before use. This product contains Vitamin A: do not take if you are pregnant or likely to become pregnant, except on the advice of a doctor or antenatal clinic. Excessive consumption may cause laxative effects. Not suitable for children under the age of 12.",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    brand: [
+      {
+        language_tag: "en_GB",
+        value: "Vivid",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    supplier_declared_dg_hz_regulation: [
+      {
+        value: "not_applicable",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    is_expiration_dated_product: [
+      {
+        value: "false",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    generic_keyword: [
+      {
+        language_tag: "en_GB",
+        value:
+          "edible skincare, dry, serum, hydrating, dry skin, irritable skin, acne, blemishes, scars, sebum, unclogs pores, anti aging, fine lines, wrinkles, hyaluronic acid, retinol, turmeric, selenium, anti-inflammatory, moisture, healthy, younger, glowing, vibrant, collagen",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    purchasable_offer: [
+      {
+        currency: "GBP",
+        start_at: {
+          value: "2023-06-13T11:59:00.689Z",
+        },
+        end_at: {
+          value: null,
+        },
+        our_price: [
+          {
+            schedule: [
+              {
+                value_with_tax: 9.99,
+              },
+            ],
+          },
+        ],
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    is_heat_sensitive: [
+      {
+        value: "false",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    country_of_origin: [
+      {
+        value: "GB",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    product_benefit: [
+      {
+        language_tag: "en_GB",
+        value: "Skin Care",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    item_form: [
+      {
+        language_tag: "en_GB",
+        value: "Liquid",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    merchant_shipping_group: [
+      {
+        value: "legacy-template-id",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    item_name: [
+      {
+        language_tag: "en_GB",
+        value:
+          "Vivid Revive Edible Skincare Serum - Anti-Aging Skin \u2013 Niacinamide, Hyaluronic Acid, Tumeric - 30 Day Serving",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    list_price: [
+      {
+        currency: "GBP",
+        value_with_tax: "14.99",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    batteries_required: [
+      {
+        value: "false",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    flavor: [
+      {
+        language_tag: "en_GB",
+        value: "Peachy Punch",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    product_site_launch_date: [
+      {
+        value: "2023-06-13T11:59:00.689Z",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    condition_type: [
+      {
+        value: "new_new",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    number_of_items: [
+      {
+        value: "1",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    item_package_dimensions: [
+      {
+        height: {
+          unit: "centimeters",
+          value: "12.50",
+        },
+        length: {
+          unit: "centimeters",
+          value: "24.50",
+        },
+        width: {
+          unit: "centimeters",
+          value: "24.50",
+        },
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    size: [
+      {
+        language_tag: "en_GB",
+        value: "50 ml (Pack of 1)",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    directions: [
+      {
+        language_tag: "en_GB",
+        value:
+          "Shake well before use. Using the pipette, squeeze 1.4ml on to the tongue and swallow. We recommend consuming one serving (1.4ml) per day. For best results continue with this routine for a minimum of 3 months. Do not exceed recommended daily intake.",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    style: [
+      {
+        language_tag: "en_GB",
+        value: "Revive",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    unit_count: [
+      {
+        type: {
+          language_tag: "en_GB",
+          value: "millilitre",
+        },
+        value: "50.00",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    target_gender: [
+      {
+        value: "unisex",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    other_product_image_locator_8: [
+      {
+        media_location:
+          "https://m.media-amazon.com/images/I/71T2UNFl8HL.jpg",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    other_product_image_locator_7: [
+      {
+        media_location:
+          "https://m.media-amazon.com/images/I/817mPnAg-fL.jpg",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    other_product_image_locator_6: [
+      {
+        media_location:
+          "https://m.media-amazon.com/images/I/91zvHPjqHPL.jpg",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    other_product_image_locator_5: [
+      {
+        media_location:
+          "https://m.media-amazon.com/images/I/71SxWRskYYL.jpg",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    other_product_image_locator_4: [
+      {
+        media_location:
+          "https://m.media-amazon.com/images/I/81uFCveLd5L.jpg",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    other_product_image_locator_3: [
+      {
+        media_location:
+          "https://m.media-amazon.com/images/I/71OpQJKhqTL.jpg",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    other_product_image_locator_2: [
+      {
+        media_location:
+          "https://m.media-amazon.com/images/I/71bKCaP6S0L.jpg",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    other_product_image_locator_1: [
+      {
+        media_location:
+          "https://m.media-amazon.com/images/I/91ERVzlZtsL.jpg",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+    main_product_image_locator: [
+      {
+        media_location:
+          "https://m.media-amazon.com/images/I/71jc-i48wuL.jpg",
+        marketplace_id: "A1F83G8C2ARO7P",
+      },
+    ],
+  }
